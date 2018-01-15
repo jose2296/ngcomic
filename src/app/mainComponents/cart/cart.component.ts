4f737a627fcbb70ddb9af286c5b2dcf0a94381fe
jose Jerez <josejerez2296@gmail.com>
2018-01-15 21:24:36 +0100

    volume fix and start cart

--- src/app/mainComponents/cart/cart.component.ts
+++ src/app/mainComponents/cart/cart.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartComics = [{issueName: "Strange Visitor",volumeName: "Kingdom Come",img: "https://comicvine.gamespot.com/api/image/scale_small/3446484-01.jpg",price: 5},{issueName: "UNO",volumeName:"UNO",img: "https://comicvine.gamespot.com/api/image/scale_small/3446484-01.jpg",price: 5}];
+
   constructor() { }
 
   ngOnInit() {
