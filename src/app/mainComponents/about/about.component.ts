d6d23ae3d3bf2484a07de458319e2bca39ac7da5
vpcarlos <33006734+vpcarlos@users.noreply.github.com>
2018-01-19 12:24:34 +0100

    Form

--- src/app/mainComponents/about/about.component.ts
+++ src/app/mainComponents/about/about.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    valid(el): void {
+
+        let element = (<HTMLInputElement>document.getElementById(el));
+        let elementsRequired = document.getElementsByName('form1')
+        let checkbox = (<HTMLInputElement>document.getElementById('check1'));
+        let submit = (<HTMLInputElement>document.getElementById('send'))
+        let disabledCheck
+
+        /*Agregar o quitar estilos a los imputs si el campo es valido*/
+        if (element.checkValidity()) {
+            element.classList.remove('is-invalid')
+            element.classList.add('is-valid')
+        } else {
+            element.classList.add('is-invalid')
+        }
+
+        /*Recorre todos los elementos requeridos y si todos están llenos, se activa el checkbox*/
+        for (let i = 0; i < elementsRequired.length; i++) {
+            if ((<HTMLInputElement>elementsRequired[i]).value == "") {
+                submit.disabled = true
+                checkbox.checked = false
+                disabledCheck = true;
+                break
+            } else {
+                disabledCheck = false
+            }
+        }
+        checkbox.disabled = disabledCheck
+    }
+
+    /*Activa el input submit cuando se aceptan los terminos*/
+    activeSubmit(): void {
+        let check = (<HTMLInputElement>document.getElementById('check1'))
+        let submit = (<HTMLInputElement>document.getElementById('send'))
+        if (check.checked) submit.disabled = false;else submit.disabled = true
+    }
+
 
 }
