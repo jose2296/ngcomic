5c9d8df455c1c3d19a4a70b6a9960802a9754c9a
Daniel de las Heras <danielherascabrera@gmail.com>
2018-01-19 12:17:48 +0100

    Bug fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,46 +1,45 @@
-
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {SearchService} from "../_services/search.service";
+import {Router} from '@angular/router';
+import {SearchService} from '../_services/search.service';
 
 import {DataservicesService} from '../_services/dataservices.service';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-    stringSearch: string;
+  stringSearch: string;
 
 
-    countCart: number = 0;
+  countCart: number;
 
 
-    constructor(private searchService: SearchService,private router:Router,private _ds:DataservicesService) {
+  constructor(private searchService: SearchService, private router: Router, private _ds: DataservicesService) {
 
-        this.countCart = (localStorage.getItem("cart") !== "" ?  localStorage.getItem("cart").split(",").length : 0);
-
-
-        _ds.event.subscribe(()=>{
-            this.countCart = _ds.countCart;
-        })
-
-    }
+    this.countCart = (localStorage.getItem('cart') !== '' && localStorage.getItem('cart') != null  ? localStorage.getItem('cart').split(',').length : 0);
 
 
-    updateString(): void {
-        /*Modifica el valor de strignSearch del servicio*/
-        this.searchService.setStringSearch(this.stringSearch)
-        /*Enruta  al componente shop*/
-        this.router.navigate(['shop'])
+    _ds.event.subscribe(() => {
+      this.countCart = _ds.countCart;
+    });
 
-    }
-
-    ngOnInit() {
+  }
 
 
-    }
+  updateString(): void {
+    /*Modifica el valor de strignSearch del servicio*/
+    this.searchService.setStringSearch(this.stringSearch);
+    /*Enruta  al componente shop*/
+    this.router.navigate(['shop']);
+
+  }
+
+  ngOnInit() {
+
+
+  }
 
 }
