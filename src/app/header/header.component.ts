76e73b46b1991cb794d8714a1668b31e327984b2
vpcarlos <33006734+vpcarlos@users.noreply.github.com>
2018-02-22 20:47:35 +0100

    boton del menú
    Esconder el menú al clickear en un item de la lista

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,41 +3,48 @@ import {Router} from '@angular/router';
 import {SearchService} from '../_services/search.service';
 
 import {DataservicesService} from '../_services/dataservices.service';
+import {NodeData} from '@angular/core/src/view';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  stringSearch: string;
-  countCart: number;
+    stringSearch: string;
+    countCart: number;
+    buttonMenu: any;
+    navBar: any;
+
+    constructor(private searchService: SearchService, private router: Router, private _ds: DataservicesService) {
+        //variable para saber la cantidad de productos del carro
+        this.countCart = _ds.countCart;
+
+        //actualizar el valor de la cantidad de productos en el carro
+        _ds.event.subscribe(() => {
+            this.countCart = _ds.countCart;
+        });
+
+    }
 
 
-  constructor(private searchService: SearchService, private router: Router, private _ds: DataservicesService) {
-    //variable para saber la cantidad de productos del carro
-    this.countCart = _ds.countCart;
+    updateString(): void {
+        /*Modifica el valor de strignSearch del servicio*/
+        this.searchService.setStringSearch(this.stringSearch);
+        /*Enruta  al componente shop*/
+        this.router.navigate(['shop']);
 
-    //actualizar el valor de la cantidad de productos en el carro
-    _ds.event.subscribe(() => {
-      this.countCart = _ds.countCart;
-    });
+    }
 
-  }
+    closeMenu(): void {
+        this.navBar = <Node>document.getElementById('navbarColor03');
+        this.navBar.setAttribute('class', 'collapse navbar-collapse');
+    }
 
-
-  updateString(): void {
-    /*Modifica el valor de strignSearch del servicio*/
-    this.searchService.setStringSearch(this.stringSearch);
-    /*Enruta  al componente shop*/
-    this.router.navigate(['shop']);
-
-  }
-
-  ngOnInit() {
+    ngOnInit() {
 
 
-  }
+    }
 
 }
